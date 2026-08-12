f3bcfae5493c2251959cc8c024410ae5599bdbe0
idavy2028-coder <idavy2028@gmail.com>
2026-08-12 10:27:25 +0800

    build: scaffold JT gateway modules

---
+++ apps/jt-gateway/src/main/java/com/idavy/drtops/jtgateway/JtGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.idavy.drtops.jtgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JtGatewayApplication.class, args);
+    }
+}
---
+++ apps/jt-gateway/src/test/java/com/idavy/drtops/jtgateway/JtGatewayApplicationTest.java
@@ -0,0 +1,31 @@
+package com.idavy.drtops.jtgateway;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:gateway_boot;DB_CLOSE_DELAY=-1",
+        "spring.flyway.enabled=false"
+})
+class JtGatewayApplicationTest {
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void startsWithoutOpeningTheDevicePort() {
+        assertThat(environment.getProperty("jt.gateway.tcp.enabled", Boolean.class)).isFalse();
+    }
+
+    @Test
+    void reservesTheDeviceAndManagementPorts() {
+        assertThat(environment.getProperty("jt.gateway.tcp.port", Integer.class)).isEqualTo(7611);
+        assertThat(environment.getProperty("server.port", Integer.class)).isEqualTo(7612);
+        assertThat(environment.getProperty("management.endpoints.web.exposure.include"))
+                .isEqualTo("health,prometheus");
+    }
+}
